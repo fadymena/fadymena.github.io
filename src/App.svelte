6fd9ce4bffd0912a7abee51ac7bccc81fa0ca853
fadymena <fadyadel.mena@studenti.unisalento.it>
2023-07-01 16:43:02 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+  import svelteLogo from "./assets/svelte.svg";
+  import viteLogo from "/vite.svg";
+  import Counter from "./lib/Counter.svelte";
+  import Appbar from "./lib/Appbar.svelte";
+</script>
+
+<div class="appbarFull"><Appbar /></div>
+<main>
+  <div>
+    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+      <img src={viteLogo} class="logo" alt="Vite Logo" />
+    </a>
+    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
+      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+    </a>
+  </div>
+  <h1>Vite + Svelte</h1>
+
+  <div class="card">
+    <Counter />
+  </div>
+
+  <p>
+    Check out <a
+      href="https://github.com/sveltejs/kit#readme"
+      target="_blank"
+      rel="noreferrer">SvelteKit</a
+    >, the official Svelte app framework powered by Vite!
+  </p>
+
+  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
+</main>
+
+<!-- <style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em rgb(255, 0, 255));
+  }
+  .read-the-docs {
+    color: #888;
+  }
+  .appbarFull{
+    width: 100dvb;
+  }
+  /* .navbar {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 20px;
+    background-color: teal;
+    color: #fff;
+  }
+
+  .nav-links a {
+    color: #fff;
+  } */
+</style> -->
---
+++ src/lib/Appbar.svelte
@@ -0,0 +1,74 @@
+<script>
+  import image from "../assets/bomb.png";
+</script>
+
+<nav class="navbar">
+  <!-- LOGO -->
+  <div class="logo appbar">MUO</div>
+  <!-- NAVIGATION MENU -->
+  <ul class="nav-links">
+    <!-- NAVIGATION MENUS -->
+    <div class="menu">
+      <li><a href="/">Home</a></li>
+      <li><a href="/">Menu</a></li>
+      <li><a href="/">Contact</a></li>
+    </div>
+  </ul>
+</nav>
+<nav>
+  <div>
+    <img src={image} class="logo bomb" alt="bomb" />
+  </div>
+</nav>
+
+<style>
+  .logo.bomb {
+    height: 6em;
+  }
+  .logo.bomb:hover {
+    filter: drop-shadow(0 0 2em rgba(255, 255, 255, 0.708));
+  }
+  .navbar {
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    padding: 20px;
+    width: 100%;
+    height: 7dvb;
+    position: relative;
+    /* background-color: #242424; */
+    background-color: teal;
+    background-image: url("../assets/svelte.svg");
+    background-size: cover;
+    color: #fff;
+    object-fit: fill;
+  }
+
+  .nav-links a {
+    color: #fff;
+    font-size: x-large;
+  }
+  /* LOGO */
+  .logo.appbar {
+    font-size: xx-large;
+    height: max-content;
+  }
+
+  /* NAVBAR MENU */
+  .menu {
+    display: inline-flex;
+    gap: 1em;
+    font-size: 18px;
+    height: min-content;
+  }
+
+  .menu li:hover {
+    background-color: #4c9e9e;
+    border-radius: 5px;
+    transition: 0.3s ease;
+  }
+
+  .menu li {
+    padding: 5px 14px;
+  }
+</style>
